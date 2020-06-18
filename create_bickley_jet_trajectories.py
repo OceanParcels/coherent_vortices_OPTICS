78c952607b57da70b8edd6852d4a45075dc90939
David Wichmann <32638174+davidwichmann@users.noreply.github.com>
2020-06-18 14:48:10 +0200

    Update create_bickley_jet_trajectories.py

--- create_bickley_jet_trajectories.py
+++ create_bickley_jet_trajectories.py
@@ -1,4 +1,8 @@
 """
+"Ordering of trajectories reveals hierarchical finite-time coherent sets in Lagrangian particle data"
+David Wichmann, Christian Kehl, Henk A. Dijkstra, and Erik van Sebille
+questions to d.wichmann@uu.nl
+
 Create bickley-jet trajectories.
 """
 
